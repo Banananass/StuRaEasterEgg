1a3cac675ee410994d6f08283fc5705d47226d73
Nicolas Hentschel <nhentsch@hs-mittweida.de>
2026-06-17 17:43:01 +0200

    it works now trust

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="de">
 <head>
     <?php
-    ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-    error_reporting(E_ALL);
+        ini_set('display_errors', '1');
+        ini_set('display_startup_errors', '1');
+        error_reporting(E_ALL);
     ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
